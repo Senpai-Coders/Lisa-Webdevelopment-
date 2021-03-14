0f7b0b74ad65fb4f32b726278a009a5ec1319bc2
jervx <louellagracechua@gmail.com>
2021-03-14 16:40:31 +0800

    + _Classes.scss

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 
 import AppRoutes from "./Routes/Routers"
 import React, { Suspense } from "react";
-import "./Assets/Styles/_Fonts.scss";
+import "./Assets/Styles/_Classes.scss";
 import "./_App.scss"
 import Loading_Screen from "./components/Global_Component/Loading_screen/Loading_screen"
 
